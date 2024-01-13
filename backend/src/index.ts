5580402f34b68a9cbd35ac1df3bb449a72b123c3
piotr <p.szczypka.dev@gmail.com>
2024-01-13 13:21:37 +0100

    setup server

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+
+//Importing .env validation 
+
+import validateEnv from '@utils/validateEnv';
+
+//App Varaibles 
+dotenv.config()
+
+validateEnv();
+
+//intializing the express app 
+const app = express();
+
+//using the dependancies
+app.use(helmet());
+app.use(cors());
+app.use(express.json())
+
+//exporting app
+module.exports = app
